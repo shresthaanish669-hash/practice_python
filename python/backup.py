b8f8a7b841b2d8911f8551771cd292de9dee7727
Anish Shrestha <shresthaanish669@gmail.com>
2026-06-16 19:16:40 +0545

    second push

--- python/backup.py
+++ python/backup.py
@@ -1,17 +1,72 @@
-#print hello world
+# Basic  python code
 print ("hello world!")
+print("My name is anish.")
+print("My age is 20.")
+
+print("My name is anish.", "My age is 20.")
+
+print(50)
+print(50 + 50)
+
 
 #Variable
 x = 7
 print(x)
-x = float(7)
-print(x)
-y = int(9.9)
+
+y = float(7)
 print(y)
+
+z = int(9.9)
+print(z)
+
+name = "Anish"
+age = 20
+print("my name is : ", name)
+print(" my age is : ", age)
+
 a = input("Enter any number:")
 print(a)
+
 b = input("Enter your name:")
 print("Hello " + b)
+
+#arthimetic operator
+a = 10
+b = 2
+
+print( a + b )
+print( a - b )
+print( a * b )
+print( a / b )
+print( a % b ) # remainder
+print( a ** b ) # a^2
+
+# relational operators 
+a = 50
+b = 30
+
+print( a == b) #false
+print( a != b) #true
+print( a >= b) #true
+print( a > b) #true
+print( a <= b) #false
+print( a < b) #false
+
+#assignment oerators
+num = 10 
+num *= num + 20
+print("num:", num)
+
+#logical operator
+a = 50
+b = 10
+print( not False )
+print( not (a > b))
+
+value1 = True
+value2 = False
+print("And operator:", value1 and value2)
+print("OR operator:", value1 or value2)
 
 #Taking input from user
 x = int(input("Enter any number: "))
@@ -128,3 +183,10 @@ if name.lower() == "anish shrestha":
     print("Your age after 10 years is:" + str(Future))
 else:
     print("Enter again.")    
+
+# while loop
+i = 0
+while i <= 5:
+    print(i)
+    i = i + 1
+print("loop executed.")
