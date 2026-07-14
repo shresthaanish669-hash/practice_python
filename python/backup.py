38ad6378ebec346b388bd6712e7a6adf0bc747f1
Anish Shrestha <shresthaanish669@gmail.com>
2026-07-14 21:43:34 +0545

    thirteen push

--- python/backup.py
+++ python/backup.py
@@ -615,3 +615,69 @@ with open("D:\practice_python\python\File_IO\demo.txt", "r") as f:
 import os
 os.remove("sample.txt")
 
+#Object Oriented Progamming(OOPS)
+class Student:
+    name = "Anish"
+    roll_no = 3
+    semester = 4
+
+S1 = Student()
+print(S1.name)
+print(S1.roll_no)
+
+class Car:
+    color ="Black"
+    model = "BMW M5"
+
+C1 = Car()
+print(C1.model)
+
+#Constructor
+class Student:
+
+ #default constructor 
+    # def __init__(self)
+
+#Parameterized constructor
+    def __init__(self, fullname, marks):
+        self.name = fullname
+        self.marks = marks
+        print("adding new student in Database.")
+
+s1 = Student("Anish", 85)
+print(s1.name, s1.marks)
+
+s2 = Student("shree", 94)
+print(s2.name, s2.marks)
+
+#Class and object attributes
+class Employee:
+    company_name = "ABC Company"
+    name = "Anonymous" #class attr
+
+    def __init__(self, fullname, id):
+        self.name = fullname
+        self.id = id
+        print("adding new student in Database.")
+
+e1 = Employee("Anish", 11)
+print(e1.name, e1.id)
+print(e1.company_name)
+
+#Method
+class Student:
+    college_name = "ABC College"
+
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+    
+    def welcome(self): #method
+        print("Welcome,", self.name)
+
+    def get_marks(self):
+        return self.marks
+    
+s1 = Student("Anish", 89)
+s1.welcome()
+print(s1.get_marks())
