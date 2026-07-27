7955a8136c3d123fb61d815d99c1a50855760a4d
Anish Shrestha <shresthaanish669@gmail.com>
2026-07-27 21:22:38 +0545

    push

--- python/backup.py
+++ python/backup.py
@@ -862,3 +862,39 @@ print(e1.get_name()) #Getter
 
 e1.set__name("Sita") #Setter
 print(e1.get_name())
+
+#Operator and Dunder Function
+print("Anish" + "Shrestha") #Concatenation
+print(type("Anish"))
+
+print([1, 2, 3, 4] + [5, 6, 7, 8]) #Merge
+print(type([1, 2, 3, 4]))
+
+
+#Operator Overloading
+class Complex:
+    def __init__(self, real, img):
+        self.real = real
+        self.img = img
+
+    def showNumber(self):
+        print(self.real, "i +", self.img, "j")
+
+    def __add__(self, num2): #creatig dunder function such as __add__
+        newReal = self.real + num2.real
+        newImg = self.img + num2.img
+        return Complex(newReal, newImg)
+
+    def __sub__(self, num2):
+        newReal = self.real - num2.real
+        newImg = self.img - num2.img
+        return Complex(newReal, newImg)
+
+num1 = Complex(2, 4)
+num1.showNumber()
+
+num2 = Complex(4, 6)
+num2.showNumber()
+
+num3 = num1 - num2
+num3.showNumber()
